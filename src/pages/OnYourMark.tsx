5d426898a8fa19cc4540f4d2b92601f87eda72d7
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-12 14:52:36 +0900

    Fetch subjects from API for records

--- src/pages/OnYourMark.tsx
+++ src/pages/OnYourMark.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar.tsx';
+import { fetchSubject } from '../hooks/fetchSubject.ts';
 
 function OnYourMark() {
   const [subject, setSubject] = useState<string>('');
+  const [recordKeys] = useState<string[]>(['piyo', 'fuwa', 'paya']);
+  const [recordSubjects, setRecordSubjects] = useState<string[]>([]);
   const navigate = useNavigate();
 
   const handleSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,6 +17,13 @@ function OnYourMark() {
     console.log(`Create ${subject}`);
     navigate('/notes');
   };
+
+  useEffect(() => {
+    (async () => {
+      const subjects = await Promise.all(recordKeys.map(fetchSubject));
+      setRecordSubjects(subjects);
+    })();
+  }, [recordKeys]);
 
   return (
     <>
@@ -42,15 +52,15 @@ function OnYourMark() {
         <div className="flex justify-center">
           <div className="w-2/3">
             <div className="record">
-              <h2>ぴよぴよ</h2>
+              <h2>{recordSubjects[0]}</h2>
               <p>2021/04/01 12:34</p>
             </div>
             <div className="record">
-              <h2>ふかふか</h2>
+              <h2>{recordSubjects[1]}</h2>
               <p>2021/04/01 12:34</p>
             </div>
             <div className="record">
-              <h2>ぱやぱや</h2>
+              <h2>{recordSubjects[2]}</h2>
               <p>2021/04/01 12:34</p>
             </div>
           </div>
