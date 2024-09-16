3653fe9813b79cd41449272d718d6c8461e9cff4
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-16 17:20:07 +0900

    Save History on localStorage

---
+++ src/hooks/localStorage.ts
@@ -0,0 +1,13 @@
+export const setHistory = (noteKey: string, subject: string) => {
+  const historyKeys = localStorage.getItem('HistoryKeys') || JSON.stringify([]);
+  const historyKeysArray = JSON.parse(historyKeys);
+
+  historyKeysArray.unshift(noteKey);
+  if (historyKeysArray.length > 3) {
+    const oldestKey = historyKeysArray.pop();
+    localStorage.removeItem(oldestKey);
+  }
+
+  localStorage.setItem('HistoryKeys', JSON.stringify(historyKeysArray));
+  localStorage.setItem(noteKey, JSON.stringify({ subject, content: '' }));
+};
