8a0d87e5b515a115baeee9d445054b253ae65564
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-18 09:24:46 +0900

    Refactored createNote to align with API changes

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,13 +1,12 @@
 export const createNote = async (
   subject: string,
-  noteKey: string,
   content: string,
 ): Promise<Response> => {
-  return await fetch(`${import.meta.env.VITE_API_URL}/notes/create`, {
+  return await fetch(`${import.meta.env.VITE_API_URL}/notes/new`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ subject, noteKey, content }),
+    body: JSON.stringify({ subject, content }),
   });
 };
