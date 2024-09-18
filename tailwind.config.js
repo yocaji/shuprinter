f163711d7e58f3477882a4b115ace5efc31de18c
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-18 21:36:31 +0900

    Introduce Preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/preline/dist/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [require('preline/plugin'), daisyui],
   daisyui: {
     themes: ['bumblebee', 'coffee'],
     darkTheme: 'coffee',
