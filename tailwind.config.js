6c0d9f69793e36975bf4d507ea269fcb0c0a5e87
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-19 14:25:14 +0900

    Introduce web font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/preline/dist/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Zen Kurenaido', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('preline/plugin')],
 };
