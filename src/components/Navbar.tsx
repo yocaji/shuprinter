4260b082bf70baa6701eca29cbbb23cbca97bfe1
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-20 21:43:30 +0900

    Add edit subject mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-gray-800 text-sm py-3">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+    <header className="px-4 py-3 w-full flex flex-wrap sm:justify-start sm:flex-nowrap text-sm bg-gray-800">
+      <nav className="w-full max-w-screen-md mx-auto sm:flex sm:items-center sm:justify-between">
         <div className="flex items-center justify-between">
           <Link
             to="/ready"
