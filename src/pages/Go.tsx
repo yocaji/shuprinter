47f81fb6b4468430913a67307c09058d93b052db
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-20 21:43:30 +0900

    Add edit subject mode

--- src/pages/Go.tsx
+++ src/pages/Go.tsx
@@ -7,37 +7,58 @@ import { updateNote } from '../hooks/api.ts';
 function Go() {
   const location = useLocation();
   const noteIdRef = useRef<string>(location.state.id);
-  const subjectRef = useRef<string>(location.state.subject);
+  const [subject, setSubject] = useState<string>(location.state.subject);
   const [content, setContent] = useState<string>('');
+  const [subjectIsEditing, setSubjectIsEditing] = useState<boolean>(false);
 
   useEffect(() => {
     setContent(location.state.content);
   }, [location.state.content]);
 
+  const handleSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubject(e.target.value);
+  };
+  useEffect(() => {
+    saveNoteLocal(noteIdRef.current, subject, content);
+  }, [subject, content]);
+
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
   useEffect(() => {
-    saveNoteLocal(noteIdRef.current, subjectRef.current, content);
-  }, [content]);
+    saveNoteLocal(noteIdRef.current, subject, content);
+  }, [subject, content]);
 
   const handleKeyDown = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 's' && (e.metaKey || e.ctrlKey)) {
       e.preventDefault();
-      await updateNote(noteIdRef.current, subjectRef.current, content);
+      await updateNote(noteIdRef.current, subject, content);
     } else if (e.key === 'Backspace') {
       e.preventDefault();
     }
   };
 
+  const handleEditClick = () => {
+    setSubjectIsEditing(true);
+  };
+
+  const handleSubjectSaveClick = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
+    // subjectのみ更新できるようにする
+    await updateNote(noteIdRef.current, subject, content);
+    setSubjectIsEditing(false);
+  };
+
   const handleSaveClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    await updateNote(noteIdRef.current, subjectRef.current, content);
+    await updateNote(noteIdRef.current, subject, content);
   };
 
   const handleCopyClick = () => {
     navigator.clipboard
-      .writeText(content)
+      .writeText(`${subject}\n\n${content}`)
       .then(() => {
         console.log('Content copied to clipboard');
       })
@@ -49,32 +70,68 @@ function Go() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto max-w-screen-lg px-4 py-8">
-        <h2 className="text-2xl text-center mb-8">{subjectRef.current}</h2>
-        <div className="flex justify-center mb-8">
+      <div className="px-4 pt-12 pb-6 bg-stone-50">
+        {subjectIsEditing ? (
+          <div className="flex mx-auto max-w-screen-md rounded-lg gap-x-2">
+            <input
+              type="text"
+              name="subject"
+              value={subject}
+              onChange={handleSubjectChange}
+              className="py-3 px-4 block w-full text-lg text-gray-800 border border-gray-200 rounded-lg focus:outline-none"
+              required={true}
+            />
+            <button
+              type={'button'}
+              onClick={handleSubjectSaveClick}
+              className="py-3 px-4 inline-flex text-xl font-medium rounded-lg border border-transparent bg-stone-200 text-gray-500 hover:bg-stone-100 focus:outline-none focus:bg-stone-100 disabled:opacity-50 disabled:pointer-events-none"
+            >
+              <span className="i-ph-check-light" />
+            </button>
+          </div>
+        ) : (
+          <div className="flex mx-auto max-w-screen-md gap-x-1">
+            <button
+              type={'button'}
+              onClick={handleEditClick}
+              className="px-2 text-xl font-medium text-slate-600 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 disabled:opacity-50 disabled:pointer-events-none"
+            >
+              <span className="i-ph-pencil-simple-line-light" />
+            </button>
+            <h2 className="pb-1 text-xl truncate">{subject}</h2>
+          </div>
+        )}
+      </div>
+      <div className="px-4 pb-12 bg-stone-50">
+        <div className="pb-6 flex justify-center mx-auto max-w-screen-md">
           <textarea
             value={content}
             onChange={handleContentChange}
             onKeyDown={handleKeyDown}
             placeholder="内容"
-            rows={24}
+            rows={18}
             className="p-4 block w-full text-lg border border-gray-200 rounded-lg focus:outline-none"
           ></textarea>
         </div>
-        <div className="flex justify-center mb-8">
-          <button
-            onClick={handleSaveClick}
-            disabled={!content}
-            className="py-3 px-4 mr-2 inline-flex items-center text-lg text-white font-medium rounded-lg border border-transparent bg-gray-800 hover:bg-gray-700 focus:outline-none focus:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
-          >
-            保存
-          </button>
-          <button
-            onClick={handleCopyClick}
-            className="py-3 px-4 inline-flex items-center text-lg text-gray-500 font-medium rounded-lg border border-gray-200 hover:border-gray-800 hover:text-gray-800 focus:outline-none focus:border-gray-800 focus:text-gray-800"
-          >
-            コピー
-          </button>
+        <div className="flex justify-end mx-auto max-w-screen-md">
+          <div className="inline-flex rounded-lg">
+            <button
+              type="button"
+              onClick={handleSaveClick}
+              className="py-2 px-3 -ms-px text-sm inline-flex justify-center items-center gap-1 rounded-s-lg border border-gray-200 shadow-sm bg-white text-gray-800 hover:bg-gray-50 focus:z-10 focus:outline-none focus:bg-gray-50"
+            >
+              <span className="i-ph-cloud-arrow-up-light" />
+              保存する
+            </button>
+            <button
+              type="button"
+              onClick={handleCopyClick}
+              className="py-2 px-3 -ms-px text-sm inline-flex justify-center items-center gap-1 rounded-e-lg border border-gray-200 shadow-sm bg-white text-gray-800 hover:bg-gray-50 focus:z-10 focus:outline-none focus:bg-gray-50"
+            >
+              <span className="i-ph-clipboard-light" />
+              コピーする
+            </button>
+          </div>
         </div>
       </div>
     </>
