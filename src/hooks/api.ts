d5634d6d7670e94533d451fefc18d9a9a9c5b43c
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-21 14:55:28 +0900

    Create delete a selected note

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -23,6 +23,25 @@ export const createNote = async (
   }
 };
 
+export const readNotes = async (): Promise<Note[] | undefined> => {
+  let response;
+  try {
+    response = await fetch(`${import.meta.env.VITE_API_URL}/notes`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response?.ok) {
+      return await response.json();
+    } else {
+      console.log(`HTTP Response Code: ${response?.status}`);
+    }
+  } catch (error) {
+    console.log('There was an error', error);
+  }
+};
+
 export const updateNote = async (
   id: string,
   subject: string,
@@ -47,11 +66,11 @@ export const updateNote = async (
   }
 };
 
-export const readNotes = async (): Promise<Note[] | undefined> => {
+export const deleteNote = async (id: string): Promise<Note[] | undefined> => {
   let response;
   try {
-    response = await fetch(`${import.meta.env.VITE_API_URL}/notes`, {
-      method: 'GET',
+    response = await fetch(`${import.meta.env.VITE_API_URL}/notes/${id}`, {
+      method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
--- src/pages/Ready.tsx
+++ src/pages/Ready.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { Note } from '../types';
 import Navbar from '../components/Navbar.tsx';
-import { createNote, readNotes } from '../hooks/api.ts';
+import { createNote, readNotes, deleteNote } from '../hooks/api.ts';
 import { saveNoteLocal } from '../hooks/localStorage.ts';
 import Footer from '../components/Footer.tsx';
 
@@ -37,30 +37,42 @@ function Ready() {
     });
   };
 
+  const handleDeleteClick = async (id: string) => {
+    const note = await deleteNote(id);
+    if (!note) return;
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <>
       <Navbar />
       <div className="px-4 py-12 bg-stone-50">
-        <h2 className="text-3xl mb-8 flex justify-center items-end gap-3">
+        <h2 className="mb-8 flex justify-center items-end gap-3 text-3xl">
           <span className="i-ph-feather-thin text-4xl" />
           何について書きますか？
         </h2>
         <form onSubmit={handleSubmit}>
-          <div className="flex justify-center mx-auto max-w-screen-md mb-6 rounded-lg">
+          <div className="mb-6 mx-auto max-w-screen-md flex items-center rounded-lg text-lg">
             <input
               type="text"
               value={subject}
               onChange={handleSubjectChange}
               placeholder="テーマ"
-              className="py-3 px-4 block w-full text-lg text-gray-800 border border-gray-200 rounded-s-lg focus:outline-none"
+              className="py-3 px-4 block w-full
+              border border-gray-200 rounded-s-lg
+              text-gray-800 focus:outline-none"
               required={true}
             />
             <button
               type={'submit'}
               disabled={!subject}
-              className="py-3 px-4 inline-flex text-xl font-medium rounded-e-lg border border-transparent bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
+              className="py-3 px-4 rounded-e-lg
+              bg-slate-200 border border-transparent
+              hover:bg-slate-300 focus:outline-none focus:bg-slate-300
+              disabled:opacity-30 disabled:pointer-events-none"
             >
-              Start
+              <span className="i-ph-arrow-right-light" />
             </button>
           </div>
         </form>
@@ -70,23 +82,31 @@ function Ready() {
           <span className="i-ph-bookmarks-simple-thin text-4xl" />
           保存したメモ
         </h2>
-        <div className="mx-auto max-w-screen-md mb-6">
+        <div className="mb-6 mx-auto max-w-screen-md">
           {notes.map((note) => (
-            <button
-              type="button"
-              key={note.id}
-              className="w-full py-3 px-4 mb-3 rounded-lg border border-gray-200 bg-stone-100 focus:outline-none focus:bg-stone-50 hover:bg-stone-50"
-              onClick={() => handleHistoryClick(note)}
-            >
-              <div className="flex items-center justify-between w-full">
-                <span className="text-base font-normal text-gray-800">
+            <div key={note.id} className="flex mb-3 w-full">
+              <button
+                type={'button'}
+                className="py-3 px-4 w-full flex items-center justify-between gap-2 truncate
+                rounded-s-lg border border-gray-200
+                hover:bg-stone-200 focus:bg-stone-200"
+                onClick={() => handleHistoryClick(note)}
+              >
+                <span className="text-base text-gray-800 truncate">
                   {note.subject}
                 </span>
-                <span className="text-sm font-normal text-gray-500">
-                  {note.updatedAt}
-                </span>
-              </div>
-            </button>
+                <span className="text-sm text-gray-400">{note.updatedAt}</span>
+              </button>
+              <button
+                type={'button'}
+                onClick={() => handleDeleteClick(note.id)}
+                className="py-3 px-4 -ms-px rounded-e-lg
+                border border-gray-200
+                hover:bg-stone-200 focus:bg-stone-200"
+              >
+                <span className="i-ph-trash-light" />
+              </button>
+            </div>
           ))}
         </div>
       </div>
