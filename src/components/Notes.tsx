4bd2eee594e9b4ebb6ee185d5510b997886788ca
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-22 10:10:28 +0900

    Add loading

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -3,16 +3,19 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { Note } from '../types';
 import { deleteNote, readNotes } from '../hooks/api.ts';
+import LoadingNotes from './LoadingNotes.tsx';
 
 function Notes() {
   const navigate = useNavigate();
   const [notes, setNotes] = useState<Note[]>([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
       const notes = await readNotes();
       if (!notes) return;
       setNotes(notes);
+      setIsLoaded(true);
     })();
   }, []);
 
@@ -39,35 +42,41 @@ function Notes() {
   };
 
   return (
-    <div className="mb-6 mx-auto max-w-screen-md">
-      {notes.map((note) => (
-        <div key={note.id} className="flex mb-3 w-full">
-          <button
-            type={'button'}
-            className="py-3 px-4 w-full flex items-center justify-between gap-2 truncate
+    <>
+      {isLoaded ? (
+        <div className="mb-6 mx-auto max-w-screen-md">
+          {notes.map((note) => (
+            <div key={note.id} className="flex mb-3 w-full">
+              <button
+                type={'button'}
+                className="py-3 px-4 w-full flex items-center justify-between gap-2 truncate
             rounded-s-lg border border-gray-200
             hover:bg-stone-200 focus:bg-stone-200"
-            onClick={() => handleNoteClick(note)}
-          >
-            <span className="text-base text-gray-800 truncate">
-              {note.subject}
-            </span>
-            <span className="text-sm text-gray-400">
-              {dayjs(note.updatedAt).format('YYYY/MM/DD HH:mm:ss')}
-            </span>
-          </button>
-          <button
-            type={'button'}
-            onClick={() => handleDeleteClick(note.id)}
-            className="py-3 px-4 -ms-px rounded-e-lg
+                onClick={() => handleNoteClick(note)}
+              >
+                <span className="text-base text-gray-800 truncate">
+                  {note.subject}
+                </span>
+                <span className="text-sm text-gray-400">
+                  {dayjs(note.updatedAt).format('YYYY/MM/DD HH:mm:ss')}
+                </span>
+              </button>
+              <button
+                type={'button'}
+                onClick={() => handleDeleteClick(note.id)}
+                className="py-3 px-4 -ms-px rounded-e-lg
             border border-gray-200
             hover:bg-stone-200 focus:bg-stone-200"
-          >
-            <span className="i-ph-trash-light" />
-          </button>
+              >
+                <span className="i-ph-trash-light" />
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <LoadingNotes />
+      )}
+    </>
   );
 }
 
