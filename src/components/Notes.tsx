dd7909d44d8956f369695b2e2e32f9c83e1c61fd
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-23 21:55:48 +0900

    Refine Notes list layout

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,13 +1,12 @@
 import dayjs from 'dayjs';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import type { Note } from '../types';
 import { deleteNote, readNotes } from '../hooks/api.ts';
 import LoadingNotes from './LoadingNotes.tsx';
 import { AuthContextConsumer } from '../contexts/AuthContext.tsx';
 
 function Notes() {
-  const navigate = useNavigate();
   const authContext = AuthContextConsumer();
   const currentUser = authContext?.currentUser;
 
@@ -24,19 +23,17 @@ function Notes() {
       setNotes(notes);
       setIsLoaded(true);
     })();
-  }, []);
+  }, [currentUser]);
 
-  const handleNoteClick = async (note: Note) => {
-    navigate('/go', {
-      state: { id: note.id, subject: note.subject, content: note.content },
-    });
-  };
-
-  const handleDeleteClick = async (id: string) => {
+  const handleDeleteClick = async (
+    id: string,
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
     const selectedNote = notes.find((note) => note.id === id);
     if (!selectedNote) return;
     const selectedNoteUpdatedAt = dayjs(selectedNote.updatedAt).format(
-      'YYYY/MM/DD HH:mm:ss',
+      'YYYY-MM-DD HH:mm',
     );
     const isConfirmed = confirm(
       `このメモを削除しますか？\n\n${selectedNote.subject}\n\n${selectedNoteUpdatedAt}`,
@@ -46,10 +43,11 @@ function Notes() {
     if (!deletedNote) return;
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    e.currentTarget.blur();
   };
 
   return (
-    <div className="px-4 py-12 bg-stone-100">
+    <div className="px-4 py-12 bg-stone-100 text-sky-800">
       <h2 className="mb-8 mx-auto max-w-screen-md flex items-end gap-3 text-2xl">
         <span className="i-ph-notepad-thin text-3xl" />
         保存したメモ
@@ -57,31 +55,33 @@ function Notes() {
       {isLoaded ? (
         <div className="mb-6 mx-auto max-w-screen-md">
           {notes.map((note) => (
-            <div key={note.id} className="flex mb-3 w-full">
-              <button
-                type={'button'}
-                className="py-3 px-4 w-full flex items-center justify-between gap-2 truncate
-            rounded-s-lg border border-gray-200
-            hover:bg-stone-200 focus:bg-stone-200"
-                onClick={() => handleNoteClick(note)}
-              >
-                <span className="text-base text-gray-800 truncate">
-                  {note.subject}
-                </span>
-                <span className="text-sm text-gray-400">
-                  {dayjs(note.updatedAt).format('YYYY/MM/DD HH:mm:ss')}
-                </span>
-              </button>
-              <button
-                type={'button'}
-                onClick={() => handleDeleteClick(note.id)}
-                className="py-3 px-4 -ms-px rounded-e-lg
-            border border-gray-200
-            hover:bg-stone-200 focus:bg-stone-200"
-              >
-                <span className="i-ph-trash-light" />
-              </button>
-            </div>
+            <Link
+              key={note.id}
+              to={'/go'}
+              state={{
+                id: note.id,
+                subject: note.subject,
+                content: note.content,
+              }}
+              className="flex justify-between mb-3 w-full bg-stone-50 border rounded-xl hover:shadow-sm focus:outline-none focus:shadow-lg transition"
+            >
+              <div className="p-4">
+                <h3 className="mb-2 text-lg">{note.subject}</h3>
+                <p className="text-sm">
+                  {dayjs(note.updatedAt).format('YYYY-MM-DD HH:mm')}
+                </p>
+              </div>
+              <div className="">
+                <button
+                  type={'button'}
+                  onClick={(e) => handleDeleteClick(note.id, e)}
+                  className="m-2 px-2 py-1 rounded-lg
+                hover:bg-stone-200 focus:bg-stone-200"
+                >
+                  <span className="i-ph-trash-light" />
+                </button>
+              </div>
+            </Link>
           ))}
         </div>
       ) : (
