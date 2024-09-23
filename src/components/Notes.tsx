4bb280916d06e402690719dcbfcc3b48b37e33f9
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-23 23:42:17 +0900

    Create Note component

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,8 +1,7 @@
-import dayjs from 'dayjs';
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import type { Note } from '../types';
-import { deleteNote, readNotes } from '../hooks/api.ts';
+import { readNotes } from '../hooks/api.ts';
+import NoteCard from './Note.tsx';
 import LoadingNotes from './LoadingNotes.tsx';
 import { AuthContextConsumer } from '../contexts/AuthContext.tsx';
 
@@ -25,26 +24,19 @@ function Notes() {
     })();
   }, [currentUser]);
 
-  const handleDeleteClick = async (
-    id: string,
-    e: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    e.preventDefault();
-    const selectedNote = notes.find((note) => note.id === id);
-    if (!selectedNote) return;
-    const selectedNoteUpdatedAt = dayjs(selectedNote.updatedAt).format(
-      'YYYY-MM-DD HH:mm',
-    );
-    const isConfirmed = confirm(
-      `このメモを削除しますか？\n\n${selectedNote.subject}\n\n${selectedNoteUpdatedAt}`,
-    );
-    if (!isConfirmed) return;
-    const deletedNote = await deleteNote(id);
-    if (!deletedNote) return;
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    e.currentTarget.blur();
+  const handleDelete = async (id: string) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
   };
+
+  // const handleSubjectEditClick = () => {
+  //   setSubjectIsEditing(true);
+  // };
+
+  // const handleSubjectSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   await upsertNote(noteIdRef.current, subject, content);
+  //   setSubjectIsEditing(false);
+  // };
 
   return (
     <div className="px-4 py-12 bg-stone-100 text-sky-800">
@@ -55,33 +47,13 @@ function Notes() {
       {isLoaded ? (
         <div className="mb-6 mx-auto max-w-screen-md">
           {notes.map((note) => (
-            <Link
-              key={note.id}
-              to={'/go'}
-              state={{
-                id: note.id,
-                subject: note.subject,
-                content: note.content,
-              }}
-              className="flex justify-between mb-3 w-full bg-stone-50 border rounded-xl hover:shadow-sm focus:outline-none focus:shadow-lg transition"
-            >
-              <div className="p-4">
-                <h3 className="mb-2 text-lg">{note.subject}</h3>
-                <p className="text-sm">
-                  {dayjs(note.updatedAt).format('YYYY-MM-DD HH:mm')}
-                </p>
-              </div>
-              <div className="">
-                <button
-                  type={'button'}
-                  onClick={(e) => handleDeleteClick(note.id, e)}
-                  className="m-2 px-2 py-1 rounded-lg
-                hover:bg-stone-200 focus:bg-stone-200"
-                >
-                  <span className="i-ph-trash-light" />
-                </button>
-              </div>
-            </Link>
+            <NoteCard
+              id={note.id}
+              subject={note.subject}
+              content={note.content}
+              updatedAt={note.updatedAt}
+              onDelete={handleDelete}
+            />
           ))}
         </div>
       ) : (
