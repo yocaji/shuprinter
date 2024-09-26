a5a618c1ee713be921292e97be6ff3645e5a4bc7
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-26 21:39:56 +0900

    Refine app name and footer layout

--- src/components/LoginStatus.tsx
+++ src/components/LoginStatus.tsx
@@ -29,29 +29,28 @@ function LoginStatus() {
   }
 
   return (
-    <div className="flex items-center gap-3 text-sm font-medium">
-      <div className="flex">
-        <Link
-          to={'/bye'}
-          className="rounded-sm outline-2 outline-amber-300 outline-offset-2 transition duration-300
-          hover:opacity-70 focus:opacity-80 focus:outline"
-        >
-          アカウント削除
-        </Link>
-        <button
-          className="px-3 py-2 inline-flex items-center justify-center rounded-full
-          outline-2 outline-amber-300 outline-offset-2 transition duration-300
-          hover:bg-stone-100 focus:bg-stone-50 focus:outline"
-          onClick={logout}
-        >
-          ログアウト
-        </button>
-      </div>
+    <div className="flex items-center gap-1 text-sm font-medium">
       <img
-        className="inline-block size-[38px] rounded-full"
+        className="me-2 size-[32px] rounded-full"
         src={currentUser.photoURL ?? undefined}
         alt={currentUser.displayName ?? undefined}
       />
+      <button
+        className="px-3 py-1 rounded-full
+        outline-2 outline-amber-300 outline-offset-2 transition duration-300
+        hover:bg-stone-100 focus:bg-stone-50 focus:outline"
+        onClick={logout}
+      >
+        ログアウト
+      </button>
+      <Link
+        to={'/bye'}
+        className="px-3 py-1 rounded-full
+        outline-2 outline-amber-300 outline-offset-2 transition duration-300
+        hover:bg-stone-100 focus:bg-stone-50 focus:outline"
+      >
+        アカウント削除
+      </Link>
     </div>
   );
 }
