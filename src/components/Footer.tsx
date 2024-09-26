793eaacdcaad1a59f615789727c55f3dcbd1095a
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-26 21:39:56 +0900

    Refine app name and footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,39 +7,34 @@ function Footer() {
       className="px-4 py-12 w-full bg-stone-200
       font-solid text-sm text-sky-800 font-medium"
     >
-      <div className="mx-auto max-w-screen-md flex justify-between">
-        <div className="flex-col">
-          <div className="mb-6">
-            <Link
-              className="text-2xl font-bold font-logo rounded-sm
+      <div className="mx-auto max-w-screen-md flex flex-col gap-12">
+        <LoginStatus />
+        <div className="flex items-center gap-3">
+          <Link
+            className="mr-3 text-2xl font-bold font-logo rounded-sm
+            outline-2 outline-amber-300 outline-offset-2 transition duration-300
+            hover:opacity-70 focus:opacity-70 focus:outline"
+            to={'/'}
+          >
+            Shuprinter
+          </Link>
+          <Link
+            to={'/terms'}
+            className="px-3 py-1 rounded-full
+            outline-2 outline-amber-300 outline-offset-2 transition duration-300
+            hover:bg-stone-100 focus:bg-stone-50 focus:outline"
+          >
+            規約とポリシー
+          </Link>
+          <a
+            href="https://github.com/yocaji/shuprinter"
+            target="_blank"
+            className="w-8 h-8 inline-flex items-center justify-center rounded-full text-2xl
               outline-2 outline-amber-300 outline-offset-2 transition duration-300
-              hover:opacity-70 focus:opacity-70 focus:outline"
-              to={'/'}
-            >
-              Shuprinter
-            </Link>
-          </div>
-          <div className="flex items-center gap-5">
-            <Link
-              to={'/terms'}
-              className="rounded-sm outline-2 outline-amber-300 outline-offset-2 transition duration-300
-              hover:opacity-70 focus:opacity-80 focus:outline"
-            >
-              規約とポリシー
-            </Link>
-            <a
-              href="https://github.com/yocaji/sprintpost"
-              target="_blank"
-              className="w-7 h-7 inline-flex items-center justify-center text-2xl
-              rounded-full outline-2 outline-amber-300 outline-offset-2 transition duration-300
-              hover:opacity-70 focus:opacity-80 focus:outline"
-            >
-              <span className="i-fa6-brands-github" />
-            </a>
-          </div>
-        </div>
-        <div>
-          <LoginStatus />
+            hover:bg-stone-100 focus:bg-stone-50 focus:outline"
+          >
+            <span className="i-fa6-brands-github" />
+          </a>
         </div>
       </div>
     </footer>
